dbc0c35281ce6d9a486af894e2e8a19b1bfb3a76
MilkyWay-U <rladbrud0302@naver.com>
2022-03-04 21:18:19 +0900

    3-7

--- frontend/python/hello.py
+++ frontend/python/hello.py
@@ -4,4 +4,9 @@ import requests
 r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
 rjson = r.json()
 
-print(rjson['RealtimeCityAir']['row'][0]['MSRSTE_NM'])
+gus = rjson['RealtimeCityAir']['row']
+
+for gu in gus:
+    gu_name = gu['MSRSTE_NM']
+    gu_mise = gu['IDEX_MVL']
+    print(gu_name,gu_mise)
