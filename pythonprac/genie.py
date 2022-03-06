79c38cb1aa77b734eec9f01d357c35cfe44fea01
MilkyWay-U <rladbrud0302@naver.com>
2022-03-06 19:38:16 +0900

    hw3

--- pythonprac/genie.py
+++ pythonprac/genie.py
@@ -12,3 +12,12 @@ headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for tr in trs:
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    rank= tr.select_one('td.number').text[0:2].strip()
+    artist= tr.select_one('td.info > a.artist.ellipsis').text.strip()
+
+    print(rank,title,artist)
