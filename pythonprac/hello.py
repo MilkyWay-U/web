4da63478dd774aaf3a84bdccdc3e771e564f36f9
MilkyWay-U <rladbrud0302@naver.com>
2022-03-06 19:04:15 +0900

    3-13

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+from pymongo import MongoClient
+# pymongo를 쓰겠슴둥
+client = MongoClient('localhost', 27017)
+# 내 컴터에 지금 돌아가고 있는 mongoDB에 접속할 거임
+db = client.dbsparta
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
@@ -17,5 +24,11 @@ for tr in trs:
         rank = tr.select_one('td:nth-child(1) > img')['alt']
         title = a_tag.text
         star = tr.select_one('td.point').text
-        print(rank, title, star)
+        doc = {
+            'rank':rank,
+            'title':title,
+            'star': star
+        }
+        db.movies.insert_one(doc)
+
         
