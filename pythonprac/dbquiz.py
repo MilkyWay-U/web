a11b7d1919b008e358ca6e50ab76cfdab3f02f22
MilkyWay-U <rladbrud0302@naver.com>
2022-03-06 19:12:27 +0900

    dbquiz_finish

--- pythonprac/dbquiz.py
+++ pythonprac/dbquiz.py
@@ -5,8 +5,4 @@ client = MongoClient('localhost', 27017)
 db = client.dbsparta
 # dbsparta 라고 하는 DB 이름으로 접속할 겁니다. (없으면 자동으로 만들어짐)
 
-target_movie = db.movies.find_one({'title':'매트릭스'})
-target_star = target_movie['star']
-movies = list(db.movies.find({'star':target_star}))
-for movie in movies:
-    print(movie['title'])
+db.movies.update_one({'title':'매트릭스'},{'$set': {'star': '0'}})
