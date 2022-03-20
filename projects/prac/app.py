502a29bea4c36ff30d837eca3c39336afd0dbdae
MilkyWay-U <rladbrud0302@naver.com>
2022-03-20 14:23:53 +0900

    render_template

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return '<button>나는 버튼이다</button>'
+   return render_template('index.html')
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
