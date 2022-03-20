6111267bd483f77f5a1b1071335b957df0483cff
MilkyWay-U <rladbrud0302@naver.com>
2022-03-20 14:19:35 +0900

    flask_button

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
-
+   return '<button>나는 버튼이다</button>'
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
